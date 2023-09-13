45b35c7318d1299005c60064f178ab84898233bd
tyler <@tylerolenak@gmail.com>
2023-09-12 17:45:16 -0700

    Fetching book Data

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,16 @@
 
 // Your Code Here
+
+let response = await fetch ("http://localhost:3001/updateBook", {
+    method: "Patch", 
+    headers: {
+        'Content-Type': 'application/json'
+       }
+    body:JSON.stringify ( {
+        "id": 3, 
+        'title': "Lengends of Arathrae"
+}), 
+})
+
+let updatedBook = await response.json():
+console.log(updatedBook) 
